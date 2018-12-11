3f5af502fc9759b4f8d9e6421696bbf8d2f3fdc9
sojwolf <sojwolf@posteo.net>
2018-12-11 17:36:25 +0100

    fixed documentation

--- R/vcfTGenotypeMatrix.R
+++ R/vcfTGenotypeMatrix.R
@@ -1,7 +1,7 @@
 #' VCF file to genotype matrix
 #'
 #' Load vcf file into R and extract a genotype matrix coded with 0,1,2
-#' @param vcf_file
+#' @param vcf_file VCF file with genotypes only (i.e. 0/0, 0/1, etc.). Missing values can be coded as "." or "./."
 #' @param max_missing_snp Set a max. percentage of missingness per SNP allowed. Default is set to 1 (i.e. 100 percent)
 #' @param max_missing_sample Set a max. percentage of missingness per sample allowed.  Default is set to 1 (i.e. 100 percent)
 #' @return matrix (sample x snp) coded 0,1,2, or NA
